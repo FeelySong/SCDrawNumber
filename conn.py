0249db9a5ccafacee0082253255232e8f7ae77d4
Feely <feelysong@gmail.com>
2014-11-21 10:39:46 +0800

    20141121

---
+++ conn.py
@@ -0,0 +1,197 @@
+# coding=utf-8
+
+
+import mysql.connector
+from datetime import datetime
+
+cnx=mysql.connector.connect(user='root',password='shl850325',host='54.254.174.234',database='shijue',charset='utf8')
+
+cursor=cnx.cursor()
+
+query='select * from ssc_set where lid=1'
+
+cursor.execute(query)
+
+result=cursor.fetchone()
+
+def kjdata(t2,cid,t1,t3):
+        if(t2!=""):
+            if(cid =='8' or cid=='6'): ##广东11选5 或者 11运夺金
+                n1=int(t2[0:2])
+                n2=int(t2[2:4])
+                n3=int(t2[4:6])
+                n4=int(t2[6:8])
+                n5=int(t2[8:10])
+            else:
+                t4=','.join(t2)
+                n1=t4[0]
+                n2=t4[2]
+                n3=t4[4]
+                if(cid!=5 and cid!=9):
+                    n4=t4[6]
+                    n5=t4[8]
+                print t4
+                n1=int(n1)
+                n2=int(n2)
+                n3=int(n3)
+                n4=int(n4)
+                n5=int(n5)
+        # echo Get_lottery($cid)."第".$t1."期:".$t2."<br>";
+        sql = 'select * from ssc_data where cid=%s and issue=%s'
+        #echo $sql."<br>";
+        cursor.execute(sql,(cid,t1))
+        rowa = cursor.fetchone()
+        tts = 0
+        if(cid==1 or cid==2 or cid==3 or cid==4):
+            if (n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts = 1
+            if (n1>9 or n2>9 or n3>9 or n4>9 or n5>9):
+                tts=1
+        elif(cid==5):
+            if (n1=="0" and n2=="0" and n3=="0"):
+                tts=1
+            if (n1>9 or n2>9 or n3>9):
+                tts=1
+        elif(cid==6 or cid==7 or cid==8 or cid==11):
+            if (n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts=1
+            if (n1>11 or n2>11 or n3>11 or n4>11 or n5>11):
+                tts=1
+        elif(cid==9):
+            if(n1=="0" and n2=="0" and n3=="0"):
+                tts=1
+            if(n1>9 or n2>9 or n3>9):
+                tts=1
+        elif(cid==10):
+            if(n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts=1
+            if(n1>9 or n2>9 or n3>9 or n4>9 or n5>9):
+                tts=1
+        name=cidname(cid)
+        print cid,name,t1,t2
+        #tmp='set cid='+cid+' name='+Get_lottery(cid), issue=t1, code='".$t2."', n1='".$n1."', n2='".$n2."', n3='".$n3."', n4='".$n4."', n5='".$n5."',opentime='".$t3."', addtime='".date("Y-m-d H:i:s")."'";
+        if(rowa is None):
+            if(tts==0):
+                sql='INSERT INTO ssc_data(cid,name,issue,code,n1,n2,n3,n4,n5,opentime,addtime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                pra=(cid,name,t1,t2,n1,n2,n3,n4,n5,t3,datetime.now())
+                cursor.execute(sql,pra)
+                t1=t1[2:]
+                print n1,n2,n3,n4,n5,cid,t1
+                zt = '1'
+                php(str(n1),str(n2),str(n3),str(n4),str(n5),str(cid),str(t1),zt)
+        else:
+            if(rowa[10]!='1'):
+                if(rowa[13]>5):
+                    pass
+                else:
+                    sqls='update ssc_data set sign=sign+1 where id ='+str(rowa[0])
+                    cursor.execute(sqls)
+        return t1
+
+#匹配彩种名称
+def cidname(x):
+    return {
+        '1': '重庆时时彩',
+        '4': '江西时时彩',
+        '6': '十一运夺金',
+        '8': '广东11选5',
+    }[x]
+
+'''
+def kjdata360(t2,cid,t1,t3):
+        if(t2!=""):
+            t4=','.join(t2)
+            print t4
+            n1=t4[0]
+            n2=t4[4]
+            n3=t4[8]
+            if(cid!=5 and cid!=9):
+                n4=t4[12]
+                n5=t4[16]
+        n1=int(n1)
+        n2=int(n2)
+        n3=int(n3)
+        n4=int(n4)
+        n5=int(n5)
+        # echo Get_lottery($cid)."第".$t1."期:".$t2."<br>";
+        sql = 'select * from ssc_data where cid=%s and issue=%s'
+        print sql
+        #echo $sql."<br>";
+        cursor.execute(sql,(cid,t1))
+        rowa = cursor.fetchone()
+        print rowa
+        tts = 0
+        if(cid==1 or cid==2 or cid==3 or cid==4):
+            if (n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts = 1
+            if (n1>9 or n2>9 or n3>9 or n4>9 or n5>9):
+                tts=1
+        elif(cid==5):
+            if (n1=="0" and n2=="0" and n3=="0"):
+                tts=1
+            if (n1>9 or n2>9 or n3>9):
+                tts=1
+        elif(cid==6 or cid==7 or cid==8 or cid==11):
+            if (n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts=1
+            if (n1>11 or n2>11 or n3>11 or n4>11 or n5>11):
+                tts=1
+        elif(cid==9):
+            if(n1=="0" and n2=="0" and n3=="0"):
+                tts=1
+            if(n1>9 or n2>9 or n3>9):
+                tts=1
+        elif(cid==10):
+            if(n1=="0" and n2=="0" and n3=="0" and n4=="0" and n5=="0"):
+                tts=1
+            if(n1>9 or n2>9 or n3>9 or n4>9 or n5>9):
+                tts=1
+        name='重庆时时彩'
+        print cid,name,t1,t2
+        #tmp='set cid='+cid+' name='+Get_lottery(cid), issue=t1, code='".$t2."', n1='".$n1."', n2='".$n2."', n3='".$n3."', n4='".$n4."', n5='".$n5."',opentime='".$t3."', addtime='".date("Y-m-d H:i:s")."'";
+        if(rowa is None):
+            if(tts==0):
+                sql='INSERT INTO ssc_data(cid,name,issue,code,n1,n2,n3,n4,n5,opentime,addtime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+                pra=(cid,name,t1,t2,n1,n2,n3,n4,n5,t3,datetime.now())
+                cursor.execute(sql,pra)
+                t1=t1[2:]
+                print n1,n2,n3,n4,n5,cid,t1
+                zt = '1'
+                php(str(n1),str(n2),str(n3),str(n4),str(n5),str(cid),str(t1),zt)
+        else:
+            if(rowa[10]!='1'):
+                if(rowa[13]>5):
+                    pass
+                else:
+                    sqls='update ssc_data set sign=sign+1 where id ='+str(rowa[0])
+                    cursor.execute(sqls)
+        return t1
+'''
+
+import subprocess
+import os
+def php(n1,n2,n3,n4,n5,cid,t1,zt):
+
+    para=n1+' '+n2+' '+n3+' '+n4+' '+n5+' '+cid+' '+t1+' '+zt
+    print para
+    para1='php -f /Users/Feely/Documents/Develop/PHP/autokj/autokj.php '
+    # open process
+    p = subprocess.Popen([para1+para], shell=True,stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+
+    # read output
+    o = p.communicate()[0]
+
+    # kill process
+    try:
+        os.kill(p.pid, os.signal.SIGTERM)
+    except:
+        pass
+    # return
+    return o
+
+#autokj($n1,$n2,$n3,$n4,$n5,$cid,$t1,1);
+# p2='58199'
+# p1='140701089'
+# p3=datetime.now()
+#
+# kjdata(t2=p2,cid='1',t1=p1,t3=p3)
---
+++ sjNumber.py
@@ -0,0 +1,177 @@
+# coding=utf-8
+__author__ = 'Feely'
+
+import time
+import multiprocessing
+import sys
+
+import DrawNO
+import conn
+
+##import GDSFC
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.excepthook = lambda *args: None
+STDERR = sys.stderr
+
+#全局期号
+CQreturndate=''     #重庆全局期号
+JXreturndate=''     #江西全局期号
+gd11_returndate=''  #11选5全局期号
+YUNreturndate=''    #11运夺金全局期号
+
+#360重庆时时彩
+def ssc360_drawnumber(ssc360_type,db_ssc_type):
+    global CQreturndate
+    while True:
+        #调用爬虫，获取开奖信息
+        assert isinstance(ssc360_type, str)
+        draw_date,draw_code_tmp, draw_time_str= DrawNO.CQ360(ssc360_type)
+        draw_code=draw_code_tmp.replace(",","")
+        if draw_code == '0' or draw_date <= CQreturndate:
+            pass
+        else:
+            returndate=conn.kjdata(t2=draw_code,cid=db_ssc_type,t1=draw_date,t3=draw_time_str)
+            time.sleep(180)
+        # draw_time = datetime.strptime(draw_time_str, "%Y-%m-%d %H:%M")
+        # ms.IsInfoExists(SPname='ibc.dbo.IsInfoExists',lottery_type=db_ssc_type,lottery_num=draw_date,kjCodes=draw_code,kjtime=draw_time,addtime=datetime.now())
+        # time.sleep(1)
+        # ms.SYSPaiJiang(SPname='ibc.dbo.SYSPaiJiang',kjExpect=draw_date,kjTime=draw_time_str,kjCode=draw_code,ltType=db_ssc_type)
+        time.sleep(30)
+
+#360江西时时彩
+def jxssc360_drawnumber(ssc360_type,db_ssc_type):
+    global JXreturndate
+    while True:
+        #调用爬虫，获取开奖信息
+        assert isinstance(ssc360_type, str)
+        draw_date,draw_code_tmp, draw_time_str= DrawNO.CQ360(ssc360_type)
+        draw_code=draw_code_tmp.replace(",","")
+        if draw_code == '0' or draw_date <= JXreturndate:
+            pass
+        else:
+            returndate=conn.kjdata(t2=draw_code,cid=db_ssc_type,t1=draw_date,t3=draw_time_str)
+            time.sleep(180)
+        # draw_time = datetime.strptime(draw_time_str, "%Y-%m-%d %H:%M")
+        # ms.IsInfoExists(SPname='ibc.dbo.IsInfoExists',lottery_type=db_ssc_type,lottery_num=draw_date,kjCodes=draw_code,kjtime=draw_time,addtime=datetime.now())
+        # time.sleep(1)
+        # ms.SYSPaiJiang(SPname='ibc.dbo.SYSPaiJiang',kjExpect=draw_date,kjTime=draw_time_str,kjCode=draw_code,ltType=db_ssc_type)
+        time.sleep(30)
+
+#360 广东11选5
+def gd11_360_drawnumber(ssc360_type,db_ssc_type):
+    global gd11_returndate
+    while True:
+        #调用爬虫，获取开奖信息
+        assert isinstance(ssc360_type, str)
+        draw_date,draw_code_tmp, draw_time_str= DrawNO.CQ360(ssc360_type)
+        draw_code=draw_code_tmp.replace(",","")
+        if draw_code == '0' or draw_date <= CQreturndate:
+            pass
+        else:
+            returndate=conn.kjdata(t2=draw_code,cid=db_ssc_type,t1=draw_date,t3=draw_time_str)
+            time.sleep(180)
+        # draw_time = datetime.strptime(draw_time_str, "%Y-%m-%d %H:%M")
+        # ms.IsInfoExists(SPname='ibc.dbo.IsInfoExists',lottery_type=db_ssc_type,lottery_num=draw_date,kjCodes=draw_code,kjtime=draw_time,addtime=datetime.now())
+        # time.sleep(1)
+        # ms.SYSPaiJiang(SPname='ibc.dbo.SYSPaiJiang',kjExpect=draw_date,kjTime=draw_time_str,kjCode=draw_code,ltType=db_ssc_type)
+        time.sleep(30)
+
+#360 11运夺金（山东11选5）
+def yun360_drawnumber(ssc360_type,db_ssc_type):
+    global YUNreturndate
+    while True:
+        #调用爬虫，获取开奖信息
+        assert isinstance(ssc360_type, str)
+        draw_date,draw_code_tmp, draw_time_str= DrawNO.CQ360(ssc360_type)
+        draw_code=draw_code_tmp.replace(",","")
+        if draw_code == '0' or draw_date <= YUNreturndate:
+            pass
+        else:
+            returndate=conn.kjdata(t2=draw_code,cid=db_ssc_type,t1=draw_date,t3=draw_time_str)
+            time.sleep(180)
+        # draw_time = datetime.strptime(draw_time_str, "%Y-%m-%d %H:%M")
+        # ms.IsInfoExists(SPname='ibc.dbo.IsInfoExists',lottery_type=db_ssc_type,lottery_num=draw_date,kjCodes=draw_code,kjtime=draw_time,addtime=datetime.now())
+        # time.sleep(1)
+        # ms.SYSPaiJiang(SPname='ibc.dbo.SYSPaiJiang',kjExpect=draw_date,kjTime=draw_time_str,kjCode=draw_code,ltType=db_ssc_type)
+        time.sleep(30)
+
+#重庆时时彩
+def ssc_drawnumber(ssc_type,db_ssc_type):
+    global CQreturndate
+    while True:
+        #调用爬虫，获取开奖信息
+        assert isinstance(ssc_type, str)
+        draw_date,draw_code, draw_time_str= DrawNO.drawnumber(ssc_type)
+        if draw_code == '0' or draw_date <= CQreturndate:
+            pass
+        else:
+            returndate=conn.kjdata(t2=draw_code,cid=db_ssc_type,t1=draw_date,t3=draw_time_str)
+            time.sleep(180)
+        # draw_time = datetime.strptime(draw_time_str, "%Y-%m-%d %H:%M")
+        # ms.IsInfoExists(SPname='ibc.dbo.IsInfoExists',lottery_type=db_ssc_type,lottery_num=draw_date,kjCodes=draw_code,kjtime=draw_time,addtime=datetime.now())
+        # time.sleep(1)
+        # ms.SYSPaiJiang(SPname='ibc.dbo.SYSPaiJiang',kjExpect=draw_date,kjTime=draw_time_str,kjCode=draw_code,ltType=db_ssc_type)
+        time.sleep(30)
+
+
+def main():
+    """
+    :rtype : Null
+    """
+
+    #360重庆时时彩
+    ssc360_type='ssccq'
+    db_ssc_type='1'
+    jobs=[]
+    for i in range(1):
+        p_360cq=multiprocessing.Process(name='360CQSSC',target=ssc360_drawnumber,args=(ssc360_type,db_ssc_type,))
+        jobs.append(p_360cq)
+        p_360cq.start()
+        p_360cq.join(timeout=10)
+
+    #360江西时时彩
+    ssc360_type='sscjx'
+    db_ssc_type='4'
+    jobs=[]
+    for i in range(1):
+        p_360jx=multiprocessing.Process(name='360JXSSC',target=ssc360_drawnumber,args=(ssc360_type,db_ssc_type,))
+        jobs.append(p_360jx)
+        p_360jx.start()
+        p_360jx.join(timeout=10)
+
+    #360 十一选五时时彩
+    gd11_360_type='gd11'
+    db_ssc_type='8'
+    jobs=[]
+    for i in range(1):
+        p_360gd11=multiprocessing.Process(name='360SYXW',target=gd11_360_drawnumber,args=(gd11_360_type,db_ssc_type,))
+        jobs.append(p_360gd11)
+        p_360gd11.start()
+        p_360gd11.join(timeout=10)
+
+    #360 十一运夺金（山东11选5）
+    yun11_360_type='yun11'
+    db_ssc_type='6'
+    jobs=[]
+    for i in range(1):
+        p_360yun11=multiprocessing.Process(name='360YUN11',target=yun360_drawnumber,args=(yun11_360_type,db_ssc_type,))
+        jobs.append(p_360yun11)
+        p_360yun11.start()
+        p_360yun11.join(timeout=10)
+
+    #重庆时时彩
+    ssc_type='cqssc'
+    db_ssc_type='1'
+    jobs=[]
+    for i in range(1):
+        p_cq=multiprocessing.Process(name='CQSSC',target=ssc_drawnumber,args=(ssc_type,db_ssc_type,))
+        jobs.append(p_cq)
+        p_cq.start()
+        p_cq.join(timeout=10)
+
+
+if __name__ == "__main__":
+    main()
